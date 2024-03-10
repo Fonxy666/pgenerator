1e55190420361cc43630f5416af8231b3db8be75
fonxy6 <viktor_6@windowslive.com>
2024-03-10 10:56:11 +0100

    feature: initialize Models for the application

---
+++ PGenerator/DatabaseLibrary/Model/Storage.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseLibrary.Model;
+
+public class Storage(Guid userId, string? application, string? userName, string? password)
+{
+    public Guid UserId { get; set; } = userId;
+    public string? Application { get; set; } = application;
+    public string? UserName { get; set; } = userName;
+    public string? Password { get; set; } = password;
+    public DateTime Created { get; set; } = DateTime.Now;
+}
---
+++ PGenerator/PGenerator/Service/PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,37 @@
+﻿using DatabaseLibrary.Model;
+
+namespace PGenerator.Service.PasswordGenerator;
+
+public static class PasswordGenerator
+{
+    public static string GeneratePassword()
+    {
+        const string lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        const string uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string symbolChars = "!@#$%^&*()-_=+";
+        const string numberChars = "0123456789";
+
+        var allChars = lowercaseChars + uppercaseChars + symbolChars + numberChars;
+
+        var rnd = new Random();
+        var password = new char[12];
+
+        password[0] = lowercaseChars[rnd.Next(lowercaseChars.Length)];
+        password[1] = uppercaseChars[rnd.Next(uppercaseChars.Length)];
+        password[2] = symbolChars[rnd.Next(symbolChars.Length)];
+        password[3] = numberChars[rnd.Next(numberChars.Length)];
+
+        for (var i = 4; i < 12; i++)
+        {
+            password[i] = allChars[rnd.Next(allChars.Length)];
+        }
+
+        for (var i = 0; i < password.Length; i++)
+        {
+            var randomIndex = rnd.Next(password.Length);
+            (password[i], password[randomIndex]) = (password[randomIndex], password[i]);
+        }
+
+        return new string(password);
+    }
+}
