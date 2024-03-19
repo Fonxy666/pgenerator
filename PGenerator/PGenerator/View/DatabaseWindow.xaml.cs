6b660d2cc9cfcff9ecfc8672e0194b57cf5f3d3e
fonxy6 <viktor_6@windowslive.com>
2024-03-20 00:31:58 +0100

    feature: accountdetails dashboard styling ongoing

--- PGenerator/PGenerator/View/DatabaseWindow.xaml.cs
+++ PGenerator/PGenerator/View/DatabaseWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using PGenerator.Service.InformationService;
 using PGenerator.ViewModel;
 
@@ -11,5 +12,33 @@ public partial class DatabaseWindow : Window
     {
         InitializeComponent();
         DataContext = new DatabaseViewModel(accountDetailService, userId, secretKey, iv);
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            DragMove();
+        }
+    }
+
+    private bool IsMaximized = false;
+    private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            if (IsMaximized)
+            {
+                WindowState = WindowState.Normal;
+                Width = 1080;
+                Height = 720;
+                IsMaximized = false;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                IsMaximized = true;
+            }
+        }
     }
 }
